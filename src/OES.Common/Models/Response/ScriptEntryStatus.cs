0c581fdd1198b0b5d70b001da7fa06ab945d0a8a
Jacob Shing <zhanhoshing@icloud.com>
2024-05-12 14:54:39 +0800

    docs: fix incorrect docs for ScriptEntryStatus.cs

--- src/OES.Common/Models/Response/ScriptEntryStatus.cs
+++ src/OES.Common/Models/Response/ScriptEntryStatus.cs
@@ -1,22 +1,38 @@
 namespace OES;
 
 /// <summary>
-/// The status of a specific answer script slice.
+/// The status of a specific answer script entry.
 /// </summary>
 public enum ScriptEntryStatus
 {
     /// <summary>
-    /// The slice is open for marking.
+    /// The entry is open for marking.
     /// </summary>
     Open = 0,
     
+    /// <summary>
+    /// The entry is marked and no more marking is required.
+    /// </summary>
     Marked = 1,
     
+    /// <summary>
+    /// The entry is marked as an empty script. 0 marks will be scored and no other markers will get this entry.
+    /// </summary>
     Empty = 2,
     
+    /// <summary>
+    /// There is unresolved irregularity regarding script image with this entry. Markers will not be able to mark.
+    /// </summary>
     ImageIrregularity = 3,
     
+    /// <summary>
+    /// There is unresolved irregularity regarding the marking panel, i.e., it is assigned to an incorrect panel.
+    /// Admins will need to manually re-assign the scripts to the correct panel.
+    /// </summary>
     PanelIrregularity = 4,
     
+    /// <summary>
+    /// There are other irregularities which prevent markers from marking the entry.
+    /// </summary>
     OtherIrregularity = 5,
 }
