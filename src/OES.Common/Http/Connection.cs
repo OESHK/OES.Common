8b008e82c96573bcc521451902e2ec1bdeac130c
Jacob Shing <zhanhoshing@icloud.com>
2024-06-08 20:31:32 +0800

    feat: temporary InternalSend method

--- src/OES.Common/Http/Connection.cs
+++ src/OES.Common/Http/Connection.cs
@@ -5,12 +5,36 @@ namespace OES.Internal;
 /// </summary>
 internal class Connection
 {
-    private readonly Uri         _baseAddress;
-    private readonly HttpClient  _httpClient;
-    private readonly Credentials _credentials;
+    private readonly Uri          _baseAddress;
+    private readonly HttpClient   _httpClient;
+    private readonly Session?     _session;
 
     private static Credentials _anonymousCredentials = Credentials.Anonymous;
     private const string       UserAgent             = "OES.Common/1.0.0";
+    
+    public Credentials? Credentials { get; set; }
+
+    /// <summary>
+    /// Creates a new connection instance.
+    /// </summary>
+    /// <param name="baseAddress">The base address of the OES API server.</param>
+    public Connection(Uri baseAddress) : this(baseAddress, new HttpClient())
+    {
+    }
+
+    /// <summary>
+    /// Creates a new connection instance with a cusomt <see cref="HttpClient"/>.
+    /// </summary>
+    /// <param name="baseAddress">The base address of the OES API server.</param>
+    /// <param name="httpClient">The custom <see cref="HttpClient"/>.</param>
+    public Connection(Uri baseAddress, HttpClient httpClient)
+    {
+        if (!baseAddress.IsAbsoluteUri)
+            throw new ArgumentException("Base URI must be absolute.", nameof(baseAddress));
+        
+        _baseAddress = baseAddress;
+        _httpClient = httpClient;
+    }
 
     /// <summary>
     /// Creates a new connection instance with a custom <see cref="HttpClient"/>.
@@ -18,19 +42,14 @@ internal class Connection
     /// <param name="baseAddress">The base address of the OES API server.</param>
     /// <param name="httpClient">The custom <see cref="HttpClient"/>.</param>
     /// <param name="credentials">The credentials for authorising API requests.</param>
-    public Connection(Uri baseAddress, HttpClient httpClient, Credentials credentials)
+    public Connection(Uri baseAddress, HttpClient httpClient, Credentials credentials) : this(baseAddress, httpClient)
     {
         // credentials must be of AuthenticationType.Basic i.e. login+password
         if (credentials.AuthenticationType != AuthenticationType.Basic)
             throw new ArgumentException(
                 "AuthenticationType of Credentials for creating a new Connection must be Basic.",
                 nameof(credentials));
-        if (!baseAddress.IsAbsoluteUri)
-            throw new ArgumentException("Base URI must be absolute.", nameof(baseAddress));
-        
-        _baseAddress = baseAddress;
-        _httpClient  = httpClient;
-        _credentials = credentials;
+        Credentials = credentials;
     }
 
     /// <summary>
@@ -54,5 +73,42 @@ internal class Connection
         baseAddress, new HttpClient(), new Credentials(login, password, userType, salt))
     {
     }
-    
+
+    private async Task<Response> InternalSend(Request request, Uri endpoint, AuthenticationType authType)
+    {
+        request.Headers.Add("User-Agent", UserAgent);
+        request = ApplyCredentials(request, authType);
+        var responseMessage = await _httpClient.SendAsync(request.GetHttpRequestMessage(_baseAddress, endpoint)).ConfigureAwait(false);
+
+        return await Response.GetResponseFromMessage(responseMessage).ConfigureAwait(false);
+    }
+
+    private Request ApplyCredentials(Request request, AuthenticationType authType)
+    {
+        switch (authType)
+        {
+            case AuthenticationType.Anonymous:
+                Credentials.Anonymous.ApplyCredentials(request);
+                break;
+            
+            case AuthenticationType.Basic:
+                if (Credentials is null)
+                    throw new ArgumentNullException(nameof(Credentials), "No login credentials supplied.");
+                Credentials.ApplyCredentials(request);
+                break;
+            
+            case AuthenticationType.AccessToken:
+                if (_session is null)
+                    break; // todo: obtain a new session
+                if (_session.IsExpiring())
+                    break; // todo: renew the current session
+                new Credentials(_session).ApplyCredentials(request);
+                break;
+            
+            default:
+                throw new ArgumentException("Invalid authentication type.", nameof(authType));
+        }
+
+        return request;
+    }
 }
