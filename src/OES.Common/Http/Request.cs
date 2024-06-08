b49fed293e21a2d7067500aacb1563ae9a1a7440
Jacob Shing <zhanhoshing@icloud.com>
2024-06-08 19:25:19 +0800

    feat: created method for converting Request to HttpRequestMessage

--- src/OES.Common/Http/Request.cs
+++ src/OES.Common/Http/Request.cs
@@ -6,22 +6,29 @@ namespace OES.Internal;
 internal class Request
 {
     public object Body { get; set; }
-    
-    public IDictionary<string, string> Headers { get; set; }
-    
-    public HttpMethod Method { get; set; }
-    
-    public IDictionary<string, string> Parameters { get; set; }
-    
-    public string ContentType { get; set; }
 
-    public HttpRequestMessage GetHttpRequestMessage()
+    public IDictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
+    
+    public HttpMethod Method { get; set; } = HttpMethod.Get;
+
+    public IDictionary<string, string> Parameters { get; set; } = new Dictionary<string, string>();
+
+    public string ContentType { get; set; } = "application/json";
+
+    public HttpRequestMessage GetHttpRequestMessage(Uri baseAddress, Uri endpoint)
     {
         var result = new HttpRequestMessage();
-        result.Method = Method;
+        result.Method     = Method;
+
+        if (endpoint.ToString().StartsWith('/')) // removes leading slash(es)
+            endpoint = new Uri(endpoint.ToString().TrimStart('/'));
+        result.RequestUri = new Uri(baseAddress, endpoint);
+        
         foreach (var header in Headers)
             result.Headers.Add(header.Key, header.Value);
+        
         result.Content = new StringContent(JsonConvert.SerializeObject(Body), Encoding.UTF8);
+        
         return result;
     }
 }
