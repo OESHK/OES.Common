d5e646ab3cd5a8447a721ff2ed756b3150e5d7c1
Jacob Shing <zhanhoshing@icloud.com>
2024-06-25 18:32:52 +0800

    feat: added ApiConnection wrapper for Connection

---
+++ src/OES.Common/Http/ApiConnection.cs
@@ -0,0 +1,49 @@
+namespace OES.Internal;
+
+/// <summary>
+/// A wrapper of <see cref="Connection"/> that provides type-friendly methods.
+/// </summary>
+internal class ApiConnection
+{
+    public ApiConnection(Connection connection)
+    {
+        Connection = connection;
+    }
+    
+    public Connection Connection { get; private set; }
+
+    public async Task<T> Get<T>(Uri endpoint, IDictionary<string, string>? parameters = null, string? contentType = null, AuthenticationType authType = AuthenticationType.AccessToken)
+    {
+        Ensure.ArgumentNotNull(endpoint, nameof(endpoint));
+        
+        var response = await Connection.Get<T>(endpoint, parameters, contentType, authType).ConfigureAwait(false);
+        return response.Body;
+    }
+
+    public async Task<T> Patch<T>(Uri endpoint, object body, IDictionary<string, string> parameters, string? contentType = null, AuthenticationType authType = AuthenticationType.AccessToken)
+    {
+        Ensure.ArgumentNotNull(endpoint, nameof(endpoint));
+        Ensure.ArgumentNotNull(body, nameof(body));
+
+        var response = await Connection.Patch<T>(endpoint, body, parameters, contentType, authType).ConfigureAwait(false);
+        return response.Body;
+    }
+
+    public async Task<T> Post<T>(Uri endpoint, object body, IDictionary<string, string> parameters, string? contentType = null, AuthenticationType authType = AuthenticationType.AccessToken)
+    {
+        Ensure.ArgumentNotNull(endpoint, nameof(endpoint));
+        Ensure.ArgumentNotNull(body, nameof(body));
+
+        var response = await Connection.Post<T>(endpoint, body, parameters, contentType, authType).ConfigureAwait(false);
+        return response.Body;
+    }
+
+    public async Task<T> Put<T>(Uri endpoint, object body, IDictionary<string, string> parameters, string? contentType = null, AuthenticationType authType = AuthenticationType.AccessToken)
+    {
+        Ensure.ArgumentNotNull(endpoint, nameof(endpoint));
+        Ensure.ArgumentNotNull(body, nameof(body));
+
+        var response = await Connection.Put<T>(endpoint, body, parameters, contentType, authType).ConfigureAwait(false);
+        return response.Body;
+    }
+}
