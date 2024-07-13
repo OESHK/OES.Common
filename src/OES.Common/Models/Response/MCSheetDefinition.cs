594c58d0fb20cae1261153bc8922408a2d628d90
Jacob Shing <zhanhoshing@icloud.com>
2024-07-13 23:03:14 +0800

    feat: create/delete methods for MCSheetDefinition

--- src/OES.Common/Models/Response/MCSheetDefinition.cs
+++ src/OES.Common/Models/Response/MCSheetDefinition.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Newtonsoft.Json;
 using OES.Internal;
 
@@ -34,4 +35,24 @@ public class MCSheetDefinition
     /// </summary>
     [JsonConverter(typeof(MCSheetAnswerJsonConverter))]
     public IReadOnlyCollection<MCSheetQuestionDefinition> Answers { get; }
+
+    /// <summary>
+    /// Gets an object representing a request to create MC sheet definition.
+    /// </summary>
+    /// <param name="scriptDefinitionId">The ID of the <see cref="ExaminationScriptDefinition"/> to which
+    /// the MC sheet definition belongs. Must have <see cref="ExaminationScriptDefinition.ExaminationScriptType"/>
+    /// set as <see cref="ExaminationScriptType.MCSheet"/>.</param>
+    /// <param name="panelId">The ID of the marking panel which will be responsible for marking the MC sheet.
+    /// Panel must be an MC panel.</param>
+    /// <param name="answers">The collection of MC sheet answers.</param>
+    public static CreateMCSheetDefinition ToCreate(
+        int                                     scriptDefinitionId,
+        int                                     panelId,
+        ICollection<MCSheetQuestionDefinition>? answers = null) =>
+        new CreateMCSheetDefinition(scriptDefinitionId, panelId, answers);
+
+    /// <summary>
+    /// Gets an object representing a request to delete an existing <see cref="MCSheetDefinition"/>.
+    /// </summary>
+    public DeleteObject ToDelete() => new DeleteObject(ScriptDefinitionId.ToString(CultureInfo.InvariantCulture));
 }
