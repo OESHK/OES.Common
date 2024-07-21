19c35f1e660933d8c7506c601a0b8fb7f8b2d0cb
Jacob Shing <zhanhoshing@icloud.com>
2024-07-21 19:07:38 +0800

    feat: all endpoints for mcq_defs

--- src/OES.Common/Clients/MCQuestionDefinitionsClient.cs
+++ src/OES.Common/Clients/MCQuestionDefinitionsClient.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using OES.Internal;
 
 namespace OES;
@@ -12,6 +13,34 @@ public class MCQuestionDefinitionsClient : ApiClient
     }
 
     /// <summary>
+    /// Create a new <see cref="MCQuestionDefinition"/>.
+    /// </summary>
+    /// <param name="examinationId">The ID of the examination to which the <see cref="MCQuestionDefinition"/> belongs.</param>
+    /// <param name="scriptDefinitionId">The ID of the script definition to which the <see cref="MCQuestionDefinition"/>
+    /// belongs.</param>
+    /// <param name="body">The request body.</param>
+    /// <returns>The creqted <see cref="MCQuestionDefinition"/>.</returns>
+    public Task<MCQuestionDefinition> Create(int examinationId, int scriptDefinitionId, CreateMCQuestionDefinition body)
+    {
+        return ApiConnection.Post<MCQuestionDefinition>(
+            ApiEndpoints.MCQuestionDefinitions(examinationId, scriptDefinitionId), body);
+    }
+
+    /// <summary>
+    /// Deletes an existing <see cref="MCQuestionDefinition"/>.
+    /// </summary>
+    /// <param name="examinationId">The ID of the examination to which the <see cref="MCQuestionDefinition"/> belongs.</param>
+    /// <param name="scriptDefinitionId">The ID of the script definition to which the <see cref="MCQuestionDefinition"/>
+    /// belongs.</param>
+    /// <param name="mcqDefinitionId">The ID of the <see cref="MCQuestionDefinition"/>.</param>
+    /// <returns>The status of the request.</returns>
+    public Task<HttpStatusCode> Delete(int examinationId, int scriptDefinitionId, int mcqDefinitionId)
+    {
+        return Connection.Delete(
+            ApiEndpoints.MCQuestionDefinitionById(examinationId, scriptDefinitionId, mcqDefinitionId));
+    }
+
+    /// <summary>
     /// Gets a <see cref="MCQuestionDefinition"/>.
     /// </summary>
     /// <param name="examinationId">The ID of the examination to which the <see cref="MCQuestionDefinition"/> belongs.</param>
@@ -23,5 +52,22 @@ public class MCQuestionDefinitionsClient : ApiClient
     {
         return ApiConnection.Get<MCQuestionDefinition>(
             ApiEndpoints.MCQuestionDefinitionById(examinationId, scriptDefinitionId, mcqDefinitionId));
+    }
+
+    /// <summary>
+    /// Gets a list of <see cref="MCQuestionDefinition"/>.
+    /// </summary>
+    /// <param name="examinationId">The ID of the examination to which the <see cref="MCQuestionDefinition"/> belongs.</param>
+    /// <param name="scriptDefinitionId">The ID of the script definition to which the <see cref="MCQuestionDefinition"/>
+    /// belongs.</param>
+    /// <param name="questionNumber">Filters the list by its question number.</param>
+    /// <returns>The requested list of <see cref="MCQuestionDefinition"/>s.</returns>
+    public Task<IReadOnlyCollection<MCQuestionDefinition>> GetAll(int examinationId, int scriptDefinitionId, int? questionNumber = null)
+    {
+        return ApiConnection.Get<IReadOnlyCollection<MCQuestionDefinition>>(
+            ApiEndpoints.MCQuestionDefinitions(examinationId, scriptDefinitionId),
+            questionNumber is null
+                ? null
+                : new Dictionary<string, object> { { "question_no", questionNumber } });
     }
 }
